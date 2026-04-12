44372052808c8c6024e56f624787e4ad4a263d20
matvey <kulikovmat@yamdex.ru>
2026-04-12 17:22:38 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+const char* VERSION = "1.0.0";
+
+int main() {
+    std::cout << "RealCarMonitor: Welcome!" << std::endl;
+    std::cout << "Version: " << VERSION << std::endl;
+    return 0;
+}
